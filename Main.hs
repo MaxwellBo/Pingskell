cfbe7a24f097bae1e204f1b52ebbdd0e3e1d485b
Max Bo <max@maxbo.me>
2016-10-30 21:07:29 +1000

    Basic setup

--- Main.hs
+++ Main.hs
@@ -1,4 +1,15 @@
-module Main where
+{-# LANGUAGE OverloadedStrings #-} 
+-- I think this is so that strings can be coerced into any type of string
+-- without explicit declaration (`String` bad - `Text` good)
 
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+import Web.Scotty
+
+main = scotty 3000 $ do
+  get "/" $ do
+    text "Hello World!"
+
+  get "/:device_id/:epoch_time" $ do
+    device_id <- param "device_id"
+    epoch_time <- param "epoch_time"
+
+    text $ mconcat [ device_id, " ", epoch_time ]
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Pingskell
 
 My solution to [TandaHQ/work-samples/pings](https://github.com/TandaHQ/work-samples/tree/master/pings%20(backend)). 
+
+
+
+
+`cabal run`
+
