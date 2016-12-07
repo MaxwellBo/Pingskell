56ebc93bf0625a3daffc8861c486c2b8e9ba7891
Max Bo <max@maxbo.me>
2016-12-07 17:48:21 +1000

    Get requests working well and proper

--- Main.hs
+++ Main.hs
@@ -15,14 +15,8 @@ import qualified Data.ByteString.Lazy as BL
 
 import Data.ByteString.Char8 as BE
 
-session :: Redis (Either Reply (Maybe ByteString))
-session = do
-  R.set "hello" "haskell"
-  R.get "hello"
 
-postPing :: TL.Text -> TL.Text -> Redis (Either Reply Integer)
-postPing deviceID epochTime = do
-  R.append (textToByteString deviceID) (textToByteString epochTime)
+{-- UTILS --}
 
 byteStringToText :: ByteString -> TL.Text
 byteStringToText = TLE.decodeUtf8 . BL.fromStrict
@@ -30,9 +24,25 @@ byteStringToText = TLE.decodeUtf8 . BL.fromStrict
 textToByteString :: TL.Text -> ByteString
 textToByteString = BL.toStrict . TLE.encodeUtf8
 
+
+{-- DB --}
+
+session :: Redis (Either Reply (Maybe ByteString))
+session = do
+  R.set "hello" "haskell"
+  R.get "hello"
+
+postPing :: TL.Text -> TL.Text -> Redis (Either Reply Integer)
+postPing deviceID epochTime = do
+  R.rpush (textToByteString deviceID) [(textToByteString epochTime)]
+
 getDevices :: Redis (Either Reply ([ByteString]))
 getDevices = do
   R.keys "*"
+
+getDevicePings :: TL.Text -> Redis (Either Reply [ByteString])
+getDevicePings deviceID = do
+  R.lrange (textToByteString deviceID) 0 (-1)
 
 main :: IO ()
 main = do 
@@ -50,12 +60,13 @@ main = do
 
     S.get "/:deviceID/:date" $ do
       deviceID <- param "deviceID"
-      date <- param "date"
-      text $ mconcat [ deviceID, " ", date ]
+      (date :: TL.Text) <- param "date"
+
+      (Right devicePings) <- liftIO $ (runRedis conn (getDevicePings deviceID))
+      json (fmap byteStringToText devicePings)
 
     S.get "/devices" $ do
-      devices <- liftIO $ (runRedis conn getDevices)
-      let (Right devices') = devices
-      json $ (fmap byteStringToText devices')
+      (Right devices) <- liftIO $ (runRedis conn getDevices)
+      json $ (fmap byteStringToText devices)
 
 
