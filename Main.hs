f1b28044ffcdca7bc3fbd56f481f40926d5731b5
Max Bo <max@maxbo.me>
2016-12-07 16:20:57 +1000

    Fixed the text disaster

--- Main.hs
+++ Main.hs
@@ -4,21 +4,38 @@
 import Web.Scotty as S 
 import Control.Monad.Trans
 import qualified Data.Text as T
+import qualified Data.Text.Lazy.Encoding as TLE
+
 import Database.Redis as R
-import Data.ByteString.Char8
+
+import Data.ByteString.Char8 as BE
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as BL
 
 session :: Redis (Either Reply (Maybe ByteString))
 session = do
   R.set "hello" "haskell"
   R.get "hello"
 
+postToDB :: ByteString -> ByteString -> Redis (Either Reply Integer)
+postToDB deviceID epochTime = do
+  R.append deviceID epochTime
+
+
 main :: IO ()
 main = do 
   conn <- connect defaultConnectInfo { connectHost = "127.0.0.1" }
   res <- runRedis conn session
   print res
+
   scotty 3000 $ do
-    S.get "/:device_id/:epoch_time" $ do
-      device_id <- param "device_id"
-      epoch_time <- param "epoch_time"
-      text $ mconcat [ device_id, " ", epoch_time ]
+    S.post "/:device_id/:epoch_time" $ do
+      deviceID <- param "device_id"
+      epochTime <- param "epoch_time"
+
+      let (deviceID' :: ByteString) = BL.toStrict $ TLE.encodeUtf8 $ deviceID 
+      let (epochTime' :: ByteString) = BL.toStrict $ TLE.encodeUtf8 $ epochTime
+
+      -- liftIO $ (runRedis conn (postToDB deviceID' epochTime'))
+
+      text $ mconcat [ deviceID, " ", epochTime ]
