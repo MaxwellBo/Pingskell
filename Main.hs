12d0de8d6f1498bae878bfecea8cef9e2a748b97
Max Bo <max@maxbo.me>
2016-12-10 17:11:26 +1000

    Rename pass

--- Main.hs
+++ Main.hs
@@ -31,19 +31,19 @@ type EpochTime = Integer
 
 {-- CONVERSIONS --}
 
-byteStringToText :: BC.ByteString -> TL.Text
-byteStringToText = TLE.decodeUtf8 . BL.fromStrict
+byteString2Text :: BC.ByteString -> TL.Text
+byteString2Text = TLE.decodeUtf8 . BL.fromStrict
 
-textToByteString :: TL.Text -> BC.ByteString
-textToByteString = BL.toStrict . TLE.encodeUtf8
+text2ByteString :: TL.Text -> BC.ByteString
+text2ByteString = BL.toStrict . TLE.encodeUtf8
 
-byteStringToString :: BC.ByteString -> String
-byteStringToString = TL.unpack . byteStringToText
+byteString2String :: BC.ByteString -> String
+byteString2String = TL.unpack . byteString2Text
 
 {-- TIME --}
 
---| Given a string possibly containing a ISO8601 date ("2016-12-10"), attempts
---| to convert that date into an Integer Unix timestamp
+-- Given a string possibly containing a ISO8601 date ("2016-12-10"), attempts
+-- to convert that date to an Integer Unix timestamp.
 parseISO8601 :: TL.Text -> Maybe EpochTime
 parseISO8601 string = (posixToEpoch . utcTimeToPOSIXSeconds) <$> utcTime
   where
@@ -51,26 +51,28 @@ parseISO8601 string = (posixToEpoch . utcTimeToPOSIXSeconds) <$> utcTime
     formatString = (iso8601DateFormat Nothing)
     posixToEpoch = truncate . toRational
 
---| Given a string possibly containing a Unix timestamp ("1481287522"), attempts
---| to convert that date into an Integer Unix timestamp
+-- Given a string possibly containing a Unix timestamp ("1481287522"), attempts
+-- to convert that date to an Integer Unix timestamp.
 parseEpochTime :: TL.Text -> Maybe EpochTime
 parseEpochTime = readMaybe . TL.unpack
 
-parseTime' :: TL.Text -> Maybe EpochTime
+-- Given a string containing either a ISO8601 date or a Unix timestamp,
+-- attempts to convert the string to to an Integer Unix timestamp.
+parseTimeStamp :: TL.Text -> Maybe EpochTime
 -- Pass the TL.Text argument to both functions, and return the first Just
-parseTime' = (<|>) <$> parseISO8601 <*> parseEpochTime
+parseTimeStamp = (<|>) <$> parseISO8601 <*> parseEpochTime
 
 {-- DB --}
 
---| Given a device ID, and a ping, produces a computation that 
---| "adds the value at the tail of the list stored at [the] key. 
---| If [the] key does not exist, [an] empty list [is created] before appending."
+-- Given a device ID, and a ping, produces a computation that 
+-- "adds the value at the tail of the list stored at [the] key. 
+-- If [the] key does not exist, [an] empty list [is created] before appending."
 postPing :: TL.Text -> TL.Text -> Redis (Either Reply Integer)
 postPing deviceID epochTime = do
-  R.rpush (textToByteString deviceID) [(textToByteString epochTime)]
+  R.rpush (text2ByteString deviceID) [(text2ByteString epochTime)]
 
---| A computation that retrives all key-value pairs from a database,
---| and converts their types as neccessary
+-- A computation that retrives all key-value pairs from a database,
+-- and converts their types as neccessary
 getKeyValuePairs :: Redis (Either Reply [(TL.Text, [EpochTime])])
 getKeyValuePairs = do
   (Right keys) <- (R.keys "*")
@@ -79,14 +81,14 @@ getKeyValuePairs = do
 
   values <- rights <$> (mapM getValue keys)
 
-  let devices = fmap byteStringToText keys
-  let pings = (fmap (read . byteStringToString)) <$> values
+  let devices = fmap byteString2Text keys
+  let pings = (fmap (read . byteString2String)) <$> values
 
   return $ return $ Prelude.zip devices pings
 
---| Given a connection, and a strategy that filters a list of pings into a
---| bounded time window, retrieves a mapping of devices and their respective
---| pings from the database.
+-- Given a connection, and a strategy that filters a list of pings into a
+-- bounded time window, retrieves a mapping of devices and their respective
+-- pings from the database.
 getMap :: Connection 
        -> ([EpochTime] -> [EpochTime]) 
        -> IO (Map.Map TL.Text [EpochTime])
@@ -102,16 +104,16 @@ getMap conn sliceFunc = do
 
 {-- BUSINESS LOGIC --}
 
---| Given a possible start Unix timestamp, filters the list of pings so that
---| only pings up to a day later remain.
+-- Given a possible start Unix timestamp, filters the list of pings so that
+-- only pings up to a day later remain.
 takeDaySlice :: Maybe EpochTime -> [EpochTime] -> [EpochTime]
 takeDaySlice from = takeRangeSlice from to
   where
     to = (+86400) <$> from -- a day later
 
---| Given a possible start and end Unix timestamp, filters the list of pings so
---| that only pings, including the start timestamp, up until, but not including,
---| the end timestamp remain.
+-- Given a possible start and end Unix timestamp, filters the list of pings so
+-- that only pings, including the start timestamp, up until, but not including,
+-- the end timestamp remain.
 takeRangeSlice :: Maybe EpochTime -> Maybe EpochTime -> [EpochTime] -> [EpochTime]
 takeRangeSlice (Just from) (Just to) pings = Prelude.filter predicate pings
   where
@@ -120,6 +122,7 @@ takeRangeSlice _ _ _ = []
 
 {-- ENTRY POINT --}
 
+-- Author: Max Bo (max@maxbo.me)
 main :: IO ()
 main = do 
   conn <- connect defaultConnectInfo
@@ -146,7 +149,7 @@ main = do
       from <- param "from"
       to <- param "to"
 
-      let sliceFunc = takeRangeSlice (parseTime' from) (parseTime' to)
+      let sliceFunc = takeRangeSlice (parseTimeStamp from) (parseTimeStamp to)
       map <- liftIO $ getMap conn sliceFunc
 
       json $ map
@@ -167,14 +170,14 @@ main = do
       from <- param "from"
       to <- param "to"
 
-      let sliceFunc = takeRangeSlice (parseTime' from) (parseTime' to)
+      let sliceFunc = takeRangeSlice (parseTimeStamp from) (parseTimeStamp to)
       map <- liftIO $ getMap conn sliceFunc
 
       json $ (Map.findWithDefault [] deviceID map)
 
 
     S.get "/devices" $ do
-      let getDevices = (fmap . fmap . fmap) byteStringToText (R.keys "*")
+      let getDevices = (fmap . fmap . fmap) byteString2Text (R.keys "*")
       (Right devices) <- liftIO $ (runRedis conn getDevices)
       json $ devices
 
